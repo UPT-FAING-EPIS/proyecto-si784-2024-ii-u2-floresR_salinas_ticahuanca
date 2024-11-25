8deb2b020c456f9597f6bb5b2363bb1906b7b0b2
marant7 <mf2018000597@virtual.upt.pe>
2024-11-25 18:48:43 -0500

    trabajo

--- PROYECTO_PDF/NegocioPDF/obj/Debug/net8.0/NegocioPDF.AssemblyInfo.cs
+++ PROYECTO_PDF/NegocioPDF/obj/Debug/net8.0/NegocioPDF.AssemblyInfo.cs
@@ -12,10 +12,10 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevSolutions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("Proyecto que permite realizar operaciones de fusión y corte de PDF utilizando .NE" +
-    "T Core 8.0.")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute(("Proyecto que permite realizar operaciones de fusión y corte de PDF utilizando .NE" +
+    "T Core 8.0."))]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be2d0d8318616cf54ab6f98c6e6113e58d20da53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+350b8f6ba64ea2985e03756324641bb52fe2e595")]
 [assembly: System.Reflection.AssemblyProductAttribute("NegocioPDF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NegocioPDF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
