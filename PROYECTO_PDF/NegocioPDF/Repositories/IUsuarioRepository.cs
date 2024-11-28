40111ffdaadad84c91c19b469325a7d7b3a7b520
marant7 <mf2018000597@virtual.upt.pe>
2024-11-28 05:51:24 -0500

    namespacefixed

--- PROYECTO_PDF/NegocioPDF/Repositories/IUsuarioRepository.cs
+++ PROYECTO_PDF/NegocioPDF/Repositories/IUsuarioRepository.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using NegocioPDF.Models;
 
-
+namespace NegocioPDF.Repositories
+{
 public interface IUsuarioRepository
 {
     Usuario Login(string correo, string password);
     void RegistrarUsuario(Usuario usuario);
     IEnumerable<Usuario> ObtenerUsuarios();
     Usuario ObtenerUsuarioPorId(int idUsuario);
+}
 }
